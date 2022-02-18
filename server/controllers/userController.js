cf8d5a80d2fd3fc066c3622d3a1b373677d6467f
Pavel Sumskoy <54890665+paulsumskoy@users.noreply.github.com>
2022-02-18 18:25:02 +0200

    Controllers added

---
+++ server/controllers/userController.js
@@ -0,0 +1,15 @@
+class UserController {
+    async registration(req, res) {
+
+    }
+
+    async login(req, res) {
+
+    }
+
+    async check(req, res) {
+        res.json('good')
+    }
+}
+
+module.exports = new UserController()
