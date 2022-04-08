d820eff335789b5e1c3073449b4c1d4b4055dbd1
Pavel Sumskoy <54890665+paulsumskoy@users.noreply.github.com>
2022-04-08 09:47:14 +0300

    CRUD for types, brands, devices

--- server/controllers/typeController.js
+++ server/controllers/typeController.js
@@ -9,7 +9,8 @@ class TypeController {
     }
 
     async getAll(req, res) {
-
+        const types = await Type.findAll()
+        return res.json(types)
     }
 }
 
--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express')
 const sequelize = require('./db')
 const models = require('./models/models')
 const cors = require('cors')
+const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
 
@@ -12,6 +13,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(fileUpload({}))
 app.use('/api', router)
 
 //Last middleware
