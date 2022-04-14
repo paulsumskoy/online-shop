100e47e5a3f753d275151e5ae137fa4549d06a1d
Pavel Sumskoy <54890665+paulsumskoy@users.noreply.github.com>
2022-04-14 13:01:01 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 const fileUpload = require('express-fileupload')
 const router = require('./routes/index')
 const errorHandler = require('./middleware/ErrorHandlingMiddleware')
+const path = require('path')
 
 const PORT = process.env.PORT || 5000
 
@@ -13,6 +14,7 @@ const PORT = process.env.PORT || 5000
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'static')))
 app.use(fileUpload({}))
 app.use('/api', router)
 
