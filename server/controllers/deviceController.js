2602acd30b8ddbc1ebd51eec8916af845e31a9a1
Pavel Sumskoy <54890665+paulsumskoy@users.noreply.github.com>
2022-04-15 16:53:12 +0300

    Device info

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,17 +1,27 @@
 const uuid = require('uuid')
 const path = require('path')
-const {Device} = require('../models/models')
+const {Device, DeviceInfo} = require('../models/models')
 const ApiError = require('../error/ApiError')
 
 class DeviceController {
     async create(req, res, next) {
         try {
-            const {name, price, brandId, typeId, info} = req.body
+            let {name, price, brandId, typeId, info} = req.body
             const {img} = req.files
             let fileName = uuid.v4() + ".jpg"
             img.mv(path.resolve(__dirname, '..', 'static', fileName))
-
             const device = await Device.create({name, price, brandId, typeId, img: fileName})
+
+            if (info) {
+                info = JSON.parse(info)
+                info.forEach(i =>
+                    DeviceInfo.create({
+                        title: i.title,
+                        description: i.description,
+                        deviceId: device.id
+                    })
+                )
+            }
 
             return res.json(device)
         } catch (e) {
@@ -42,8 +52,17 @@ class DeviceController {
     }
 
     async getOne(req, res) {
+        const {id} = req.params
+        const device = await Device.findOne(
+            {
+                where: {id},
+                include: [{model: DeviceInfo, as: 'info'}]
+            }
 
+        )
+        return res.json(device)
     }
+
 }
 
 module.exports = new DeviceController()
