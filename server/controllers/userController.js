78d586fcb092eafb526fc98a88f65eae6016fcbd
Pavel Sumskoy <54890665+paulsumskoy@users.noreply.github.com>
2022-04-15 17:18:58 +0300

    Minor changes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -3,6 +3,14 @@ const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 const {User, Basket} = require('../models/models')
 
+const generateJwt = (id, email, role) => {
+   return jwt.sign(
+        {id, email, role},
+        process.env.SECRET_KEY,
+        {expiresIn: '24h'}
+    )
+}
+
 class UserController {
     async registration(req, res, next) {
         const {email, password, role} = req.body
@@ -16,11 +24,7 @@ class UserController {
         const hashPassword = await bcrypt.hash(password, 5)
         const user = await  User.create({email, role, password: hashPassword})
         const basket = await Basket.create({userId: user.id})
-        const token = jwt.sign(
-            {id: user.id, email, role},
-            process.env.SECRET_KEY,
-            {expiresIn: '24h'}
-        )
+        const token = generateJwt(user.id, user.email, user.role)
         return res.json({token})
     }
 
