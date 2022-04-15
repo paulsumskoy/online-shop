248167aa760e5532b860caa521dba61734166e82
Pavel Sumskoy <54890665+paulsumskoy@users.noreply.github.com>
2022-04-15 17:15:33 +0300

    JWT token

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,27 @@
 const ApiError = require('../error/ApiError')
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+const {User, Basket} = require('../models/models')
 
 class UserController {
-    async registration(req, res) {
-
+    async registration(req, res, next) {
+        const {email, password, role} = req.body
+        if (!email || !password) {
+            return next(ApiError.badRequest('Email or password not valid'))
+        }
+        const candidate = await User.findOne({where: {email}})
+        if (candidate) {
+            return next(ApiError.badRequest('User already exist'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await  User.create({email, role, password: hashPassword})
+        const basket = await Basket.create({userId: user.id})
+        const token = jwt.sign(
+            {id: user.id, email, role},
+            process.env.SECRET_KEY,
+            {expiresIn: '24h'}
+        )
+        return res.json({token})
     }
 
     async login(req, res) {
