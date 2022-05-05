855ce652cc78c5b087f8e81874e4fbd269dd90ee
Pavel Sumskoy <54890665+paulsumskoy@users.noreply.github.com>
2022-05-05 18:39:52 +0300

    Device information page

--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
+import bigStar from '../assets/bigStar.png'
 
 const DevicePage = () => {
+    const device = {id: 1, name: '2 super', price: 10000, rating: 5}
+    const description = [
+        {id:1, title: 'disk', description: '1gb'},
+        {id:2, title: 'disk', description: '1gb'},
+        {id:3, title: 'disk', description: '1gb'},
+        {id:4, title: 'disk', description: '1gb'},
+    ]
     return (
-        <div>
-            DEVICE PAGE
-        </div>
+        <Container className="mt-3">
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={device.img}/>
+                </Col>
+                <Col md={4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h2>{device.name}</h2>
+                        <div
+                            className="d-flex align-items-center justify-content-center"
+                            style={{background: `url(${bigStar}) no-repeat center center`, width:240, height: 240, backgroundSize: 'cover', fontSize:64}}
+                        >
+                            {device.rating}
+                        </div>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
+                    >
+                        <h3>From {device.price} USD</h3>
+                        <Button variant={"outline-dark"}>Add to list</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h1>Feature</h1>
+                {description.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.title}: {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
     );
 };
 
