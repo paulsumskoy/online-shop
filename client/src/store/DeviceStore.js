1bff26ccf66d69d42d44f13c4c3607b02c8c686a
Pavel Sumskoy <54890665+paulsumskoy@users.noreply.github.com>
2022-05-07 13:08:24 +0300

    Getting info from server

--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -2,30 +2,9 @@ import {makeAutoObservable} from "mobx";
 
 export default class DeviceStore {
     constructor() {
-        this._types = [
-            {id: 1, name: 'Phone'},
-            {id: 2, name: 'Notebook'},
-            {id: 3, name: 'TV'},
-            {id: 4, name: 'Fridge'}
-        ]
-        this._brands = [
-            {id: 1, name: 'Samsung'},
-            {id: 2, name: 'Apple'},
-            {id: 3, name: 'Dell'},
-            {id: 4, name: 'MSI'},
-        ]
-        this._devices = [
-            {id: 1, name: '2 super', price: 10000, rating: 5},
-            {id: 2, name: '2 super', price: 10000, rating: 5},
-            {id: 3, name: '2 super', price: 10000, rating: 5},
-            {id: 4, name: '2 super', price: 10000, rating: 5},
-            {id: 5, name: '2 super', price: 10000, rating: 5},
-            {id: 6, name: '2 super', price: 10000, rating: 5},
-            {id: 7, name: '2 super', price: 10000, rating: 5},
-            {id: 8, name: '2 super', price: 10000, rating: 5},
-            {id: 9, name: '2 super', price: 10000, rating: 5},
-
-        ]
+        this._types = []
+        this._brands = []
+        this._devices = []
         this._selectedType = {}
         this._selectedBrand ={}
         makeAutoObservable(this)
