e4c5206ab682152c4c61c092a34a4f7a47de62a4
Pavel Sumskoy <54890665+paulsumskoy@users.noreply.github.com>
2022-05-07 13:27:29 +0300

    Device page connect to server

--- client/src/http/index.js
+++ client/src/http/index.js
@@ -9,7 +9,7 @@ const $authHost = axios.create({
 })
 
 const authInterceptor = config => {
-    config.header.authorization = `Bearer ${localStorage.getItem('token')}`
+    config.headers.authorization = `Bearer ${localStorage.getItem('token')}`
     return config
 }
 
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
 import bigStar from '../assets/bigStar.png'
+import {useParams} from "react-router-dom";
+import {fetchOneDevice} from "../http/deviceAPI";
 
 const DevicePage = () => {
-    const device = {id: 1, name: '2 super', price: 10000, rating: 5}
-    const description = [
-        {id:1, title: 'disk', description: '1gb'},
-        {id:2, title: 'disk', description: '1gb'},
-        {id:3, title: 'disk', description: '1gb'},
-        {id:4, title: 'disk', description: '1gb'},
-    ]
+    const [device, setDevice] = useState({info: []})
+    const {id} = useParams()
+
+    useEffect(() => {
+        fetchOneDevice(id).then(data => setDevice(data))
+    }, [])
+
     return (
         <Container className="mt-3">
             <Row>
                 <Col md={4}>
-                    <Image width={300} height={300} src={device.img}/>
+                    <Image width={300} height={300} src={process.env.REACT_APP_API_URL + device.img}/>
                 </Col>
                 <Col md={4}>
                     <Row className="d-flex flex-column align-items-center">
@@ -39,7 +41,7 @@ const DevicePage = () => {
             </Row>
             <Row className="d-flex flex-column m-3">
                 <h1>Feature</h1>
-                {description.map((info, index) =>
+                {device.info.map((info, index) =>
                     <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
                         {info.title}: {info.description}
                     </Row>
